74de2c40a7f481eae0c9cab3a923a05f7c04f614
parul-15 <77332227+parul-15@users.noreply.github.com>
2024-12-20 11:40:24 +0530

    "Add Employee class to firstcode.java".

--- firstcode.java
+++ firstcode.java
@@ -1,1 +1,55 @@
+public class Employee {
+    private String name;
+    private int id;
+    private String department;
+    private double salary;
 
+    // Constructor
+    public Employee(String name, int id, String department, double salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // Method to display employee details
+    public void displayEmployeeDetails() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+    }
+}
